093ca145b76619d5c229fa3b6a0b5673d994a51c
fatmaaydin1214 <fatmaaydin1214@gmail.com>
2023-05-17 19:49:38 +0200

    first commit

---
+++ src/test/java/day14_JSExecuter/C01_JavaScriptExecuter.java
@@ -0,0 +1,30 @@
+package day14_JSExecuter;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+public class C01_JavaScriptExecuter extends TestBase {
+
+    @Test
+    public void test01() throws InterruptedException {
+        //wisequarter anasayfasina gidin
+        driver.get("https://www.wisequarter.com");
+        // asagiya inerek software development engineer in test SDET
+        //kursunda explore the course butonuna basin
+
+        WebElement exploreButonu= driver.findElement(By.xpath("(//a[@class='elementskit-btn whitespace--normal'])[2]"));
+        JavascriptExecutor jse=(JavascriptExecutor) driver;
+        //jse ile butona kadar scroll yapalim
+        jse.executeScript("arguments[0].scrollIntoview",exploreButonu);
+        //jse ile click yapalim
+        jse.executeScript("arguments[0].click",exploreButonu);
+        //jse ile alert olusturma
+        jse.executeScript("alert('Bu is bu kadar')");
+
+        Thread.sleep(3000);
+
+    }
+}
